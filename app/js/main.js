dd177fa1ff8b155c20af610812461ef65f524126
slavik <slavik.zaliotnyi@gmail.com>
2022-07-18 21:01:20 +0300

    added header and adaptive header

--- app/js/main.js
+++ app/js/main.js
@@ -1,3 +1,16 @@
 $(function () {
-
+  $('.header__number-btn').on('click', function () {
+    $('.header__numbers').slideToggle(200);
+    $('.header__number-btn').toggleClass('active');
+  });
+  $('.menu__btn').on('click', function () {
+    $('.menu__list').addClass('active');
+  });
+  $('.menu__close').on('click', function () {
+    $('.menu__list').removeClass('active');
+  });
+  $('.header__categories-btn').on('click', function () {
+    $('.header__categories').slideToggle(200);
+    $('.header__categories-btn').toggleClass('active');
+  });
 })
